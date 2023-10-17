4ff62410906640442095453428f6ba5448f5701d
Jianfeng Sun <jianfeng.sunmt@gmail.com>
2023-10-17 13:01:57 +0100

    generic template for any criterion as input
    generic template for any criterion as input

--- phylotres/locus.py
+++ phylotres/locus.py
@@ -12,6 +12,7 @@ from phylotres.scenario.pcrnum.SingleLocus import SingleLocus as pcrnum
 from phylotres.scenario.amplrate.SingleLocus import SingleLocus as amplrate
 from phylotres.scenario.umilen.SingleLocus import SingleLocus as umilen
 from phylotres.scenario.seqdep.SingleLocus import SingleLocus as seqdep
+from phylotres.scenario.generic.SingleLocus import SingleLocus as generic
 
 
 def simu_seq_err(
@@ -417,6 +418,83 @@ def simu_seq_dep(
     return 'Finished!'
 
 
+def simu_generic(
+        len_params,
+        seq_num,
+        seq_len,
+        is_sv_umi_lib,
+        is_sv_seq_lib,
+        is_sv_primer_lib,
+        is_sv_adapter_lib,
+        is_sv_spacer_lib,
+        fasta_cdna_fpn,
+        working_dir,
+        condis,
+        sim_thres,
+        permutation,
+        err_route,
+        pcr_error,
+        seq_error,
+        pcr_num,
+        err_num_met,
+        use_seed,
+        seed,
+        ampl_rate,
+        sv_fastq_fp,
+        sv_fastq_fn,
+        pcr_deletion=False,
+        pcr_insertion=False,
+        pcr_del_rate=0,
+        pcr_ins_rate=0,
+        seq_deletion=False,
+        seq_insertion=False,
+        seq_del_rate=0,
+        seq_ins_rate=0,
+        seq_sub_spl_number=None,
+        seq_sub_spl_rate=1/3,
+        verbose=True,
+        **kwargs,
+):
+    generic(
+        len_params=len_params,
+        seq_params=kwargs['seq_params'] if 'seq_params' in kwargs.keys() else None,
+        seq_num=seq_num,
+        seq_len=seq_len,
+        is_sv_umi_lib=is_sv_umi_lib,
+        is_sv_seq_lib=is_sv_seq_lib,
+        is_sv_primer_lib=is_sv_primer_lib,
+        is_sv_adapter_lib=is_sv_adapter_lib,
+        is_sv_spacer_lib=is_sv_spacer_lib,
+        fasta_cdna_fpn=fasta_cdna_fpn,
+        working_dir=working_dir,
+        condis=condis,
+        sim_thres=sim_thres,
+        permutation=permutation,
+        err_route=err_route,
+        pcr_error=pcr_error,
+        seq_error=seq_error,
+        pcr_num=pcr_num,
+        err_num_met=err_num_met,
+        pcr_deletion=pcr_deletion,
+        pcr_insertion=pcr_insertion,
+        pcr_del_rate=pcr_del_rate,
+        pcr_ins_rate=pcr_ins_rate,
+        seq_deletion=seq_deletion,
+        seq_insertion=seq_insertion,
+        seq_del_rate=seq_del_rate,
+        seq_ins_rate=seq_ins_rate,
+        use_seed=use_seed,
+        seed=seed,
+        ampl_rate=ampl_rate,
+        sv_fastq_fp=sv_fastq_fp,
+        sv_fastq_fn=sv_fastq_fn,
+        seq_sub_spl_number=seq_sub_spl_number,
+        seq_sub_spl_rate=seq_sub_spl_rate,
+        verbose=verbose,
+    ).generate()
+    return 'Finished!'
+
+
 if __name__ == "__main__":
     from phylotres.path import to
     import numpy as np
@@ -724,7 +802,67 @@ if __name__ == "__main__":
     #     sv_fastq_fp=to('data/simu/'),
     # )
 
-    simu_seq_dep(
+    # simu_seq_dep(
+    #     # initial sequence generation
+    #
+    #     len_params={
+    #         'umi': {
+    #             'umi_unit_pattern': 3,
+    #             'umi_unit_len': 12,
+    #         },
+    #         'umi_1': {
+    #             'umi_unit_pattern': 3,
+    #             'umi_unit_len': 12,
+    #         },
+    #         'barcode': 16,
+    #         'seq': 100,
+    #         'seq_2': 100,
+    #         'adapter': 10,
+    #         'adapter_1': 10,
+    #         'primer': 10,
+    #         'primer_1': 10,
+    #         'spacer': 10,
+    #         'spacer_1': 10,
+    #     },
+    #     seq_params={
+    #         'custom': 'AAGC',
+    #         'custom_1': 'A',
+    #     },
+    #     seq_num=50,
+    #     seq_len=100,
+    #     working_dir=to('data/simu/'),
+    #     fasta_cdna_fpn=False,
+    #     # fasta_cdna_fpn=to('data/Homo_sapiens.GRCh38.cdna.all.fa.gz'),
+    #
+    #     is_sv_umi_lib=True,
+    #     is_sv_seq_lib=True,
+    #     is_sv_primer_lib=True,
+    #     is_sv_adapter_lib=True,
+    #     is_sv_spacer_lib=True,
+    #     condis=['umi'],
+    #     # condis=['umi', 'seq'],
+    #     # condis=['umi', 'custom', 'seq', 'custom_1'],
+    #     sim_thres=3,
+    #     permutation=0,
+    #
+    #     # PCR amplification
+    #     ampl_rate=0.9,
+    #     err_route='minnow',  # tree minnow err1d err2d mutation_table_minimum mutation_table_complete
+    #     pcr_error=1e-04,
+    #     pcr_num=10,
+    #     err_num_met='nbinomial',
+    #     seq_error=0.01,
+    #     seq_sub_spl_numbers=[100, 500, 1000, 10000],  # None 200
+    #     # seq_sub_spl_rate=0.333,
+    #     use_seed=True,
+    #     seed=1,
+    #
+    #     verbose=False,  # True False
+    #
+    #     sv_fastq_fp=to('data/simu/'),
+    # )
+
+    simu_generic(
         # initial sequence generation
 
         len_params={
@@ -774,7 +912,7 @@ if __name__ == "__main__":
         pcr_num=10,
         err_num_met='nbinomial',
         seq_error=0.01,
-        seq_sub_spl_numbers=[100, 500, 1000, 10000],  # None 200
+        seq_sub_spl_number=2000,  # None 200
         # seq_sub_spl_rate=0.333,
         use_seed=True,
         seed=1,
@@ -782,4 +920,5 @@ if __name__ == "__main__":
         verbose=False,  # True False
 
         sv_fastq_fp=to('data/simu/'),
+        sv_fastq_fn='example',
     )
