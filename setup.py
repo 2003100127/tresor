91ac73c44697e3faf58deed7b3141f4e4120f7c6
Jianfeng Sun <jianfeng.sunmt@gmail.com>
2024-04-03 18:25:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="phylotres",
+    name="tresor",
     version="0.0.1",
-    keywords=["conda", "phylotres"],
-    description="PhyloTres",
-    long_description="Simulation tool PhyloTres",
+    keywords=["conda", "tresor"],
+    description="Tresor",
+    long_description="Simulation tool Tresor",
     license="GNU GENERAL V3.0",
 
-    url="https://github.com/cribbslab/phylotres, https://github.com/2003100127",
+    url="https://github.com/cribbslab/tresor, https://github.com/2003100127",
     author="Jianfeng Sun",
     author_email="jianfeng.sun@ndorms.ox.ac.uk, adam.cribbs@ndorms.ox.ac.uk",
 
@@ -25,12 +25,12 @@ setup(
         "biopython",
         "tables",
         "pyyaml",
-        "rpy2", # 3.4.5
+        # "rpy2", # 3.4.5
         "pyfiglet==0.8.post1",
     ],
     entry_points={
         'console_scripts': [
-            'phylotres=phylotres.main:main',
+            'tresor=tresor.main:main',
         ],
     },
 )
